fd9c0f975eeadd8c189b2f5e015d57ae321885dd
Adam S <adam.sarli@gmail.com>
2019-03-21 19:24:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = kickstarter.css("li.project.grid_4")
   #project_title = projects.css("h2.bbcard_name strong a").text
+  image_link = projects.css("div.project-thumbnail a img").attribute("src").value
   
   
   binding.pry
